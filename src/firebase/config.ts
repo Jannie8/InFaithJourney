f523cfbc7e0f8e2fbbded9fc874dbdafc0c9312c
Jordan Curtis <j.curtis@industrialgrowthhub.com>
2026-04-30 15:03:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2474367430-9a561",
+  "appId": "1:392786987548:web:96ea19cb3d315fc31fedd9",
+  "apiKey": "AIzaSyAWTaqa1Cfq28rtyBDRTKNwtHYzsi780WQ",
+  "authDomain": "studio-2474367430-9a561.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "392786987548"
+};
