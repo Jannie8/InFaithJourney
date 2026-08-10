9f33eea5e717bbc8c77c1eba8381b805775e5bed
Jannie8 <janniesaayman88@gmail.com>
2026-08-10 15:45:29 +0200

    fixed

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyDAJGl6Sku_DUXGozZqGcYLbZKVEoPCdF8",
-  "authDomain": "infaithjourney-90d96.firebaseapp.com",
-  "projectId": "infaithjourney-90d96",
-  "storageBucket": "infaithjourney-90d96.firebasestorage.app",
-  "messagingSenderId": "390734989552",
-  "appId": "1:390734989552:web:8bf6797e71031ae3371a35",
-  "measurementId": "G-DBWPSTVJQJ"
+  apiKey: "AIzaSyDAJGl6Sku_DUXGozZqGcYLbZKVEoPCdF8",
+  authDomain: "infaithjourney-90d96.firebaseapp.com",
+  projectId: "infaithjourney-90d96",
+  storageBucket: "infaithjourney-90d96.firebasestorage.app",
+  messagingSenderId: "390734989552",
+  appId: "1:390734989552:web:8bf6797e71031ae3371a35",
+  measurementId: "G-DBWPSTVJQJ"
 };
